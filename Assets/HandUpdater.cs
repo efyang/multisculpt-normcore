dcff1c734e1cdeb9afb95dd1f5b073576f408487
Edward Yang <edward.yang6771@gmail.com>
2022-04-12 15:15:20 -0400

    Test host setting

---
+++ Assets/HandUpdater.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Normal.Realtime;
+
+public class HandUpdater : MonoBehaviour
+{
+    // Reference to Realtime to use to instantiate brush strokes
+    [SerializeField] private Realtime _realtime;
+    // Which hand should this brush instance track?
+    private enum Hand { LeftHand, RightHand };
+    [SerializeField] private Hand _hand = Hand.RightHand;
+
+    // Used to keep track of the current brush tip position and the actively drawing brush stroke
+    private Vector3     _handPosition;
+    private Quaternion  _handRotation;
+    private BrushStroke _activeBrushStroke;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Start by figuring out which hand we're tracking
+        XRNode node    = _hand == Hand.LeftHand ? XRNode.LeftHand : XRNode.RightHand;
+        string trigger = _hand == Hand.LeftHand ? "Left Trigger" : "Right Trigger";
+
+        // Get the position & rotation of the hand
+        bool handIsTracking = UpdatePose(node, ref _handPosition, ref _handRotation);
+
+        // Figure out if the trigger is pressed or not
+        bool triggerPressed = Input.GetAxisRaw(trigger) > 0.1f;
+
+        // If we lose tracking, stop drawing
+        if (!handIsTracking)
+            triggerPressed = false;
+    }
+
+    //// Utility
+
+    // Given an XRNode, get the current position & rotation. If it's not tracking, don't modify the position & rotation.
+    private static bool UpdatePose(XRNode node, ref Vector3 position, ref Quaternion rotation) {
+        List<XRNodeState> nodeStates = new List<XRNodeState>();
+        InputTracking.GetNodeStates(nodeStates);
+
+        foreach (XRNodeState nodeState in nodeStates) {
+            if (nodeState.nodeType == node) {
+                Vector3    nodePosition;
+                Quaternion nodeRotation;
+                bool gotPosition = nodeState.TryGetPosition(out nodePosition);
+                bool gotRotation = nodeState.TryGetRotation(out nodeRotation);
+
+                if (gotPosition)
+                    position = nodePosition;
+                if (gotRotation)
+                    rotation = nodeRotation;
+
+                return gotPosition;
+            }
+        }
+
+        return false;
+    }
+}
